a1fb0b379309979b3d7362f6157eba1a6f646d32
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-21 10:45:39 -0300

    Refactor TitleState and add enter input handling
    Refactor TitleState to improve structure and add input handling.

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -1,44 +1,84 @@
 package states;
 
 import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.FlxG;
 import flxanimate.FlxAnimatedSprite;
 
-var logo:FlxAnimatedSprite;
-var gfDanceTitle:FlxAnimatedSprite;
-var titleEnter:FlxAnimatedSprite;
-var gfDanceTitlebg:FlxSprite;
+#if mobile
+import flixel.input.touch.FlxTouch;
+#end
 
 class TitleState extends FlxState
 {
-    override public function create():Void
-    {
-    gfDanceTitle = new FlxAnimatedSprite(0, 0, "assets/images/menus/titlescreen/gfDanceTitle.png");
-    gfDanceTitle.loadGraphic("assets/images/menus/titlescreen/gfDance.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
-    gfDanceTitle.addAnimation("gfDance", [0, 1, 2, 3, 4, 5], 12, true);
-    gfDanceTitle.play("gfDance");
-    add(gfDanceTitle);
+	var logo:FlxAnimatedSprite;
+	var gfDanceTitle:FlxAnimatedSprite;
+	var titleEnter:FlxAnimatedSprite;
+	var gfDanceTitlebg:FlxSprite;
 
-    logo = new FlxAnimatedSprite(0, 0, "assets/images/menus/titlescreen/logo.png");
-    logo.loadGraphic("assets/images/menus/titlescreen/logo.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
-    logo.addAnimation("logo bumpin", [0, 1, 2, 3, 4, 5], 12, true);
-    logo.play("logo bumpin");
-    add(logo);
-    
-    titleEnter = new FlxAnimatedSprite(0, 0, "assets/images/menus/titlescreen/titleEnter.png");
-    titleEnter.loadGraphic("assets/images/menus/titlescreen/titleEnter.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
-    if (pressed != null)
-    {
-        titleEnter.addAnimation("ENTER PRESSED", [0, 1, 2, 3, 4, 5], 12, true);
-        titleEnter.play("ENTER PRESSED");
-    }
-    if (not pressed != null)
-    {
-        titleEnter.addAnimation("ENTER FREEZE", [0], 12, true);
-        titleEnter.play("ENTER FREEZE");
-    }
-    add(titleEnter);
+	var pressedEnter:Bool = false;
 
-    gfDanceTitlebg = new FlxSprite(0, 0, "assets/images/menus/titlescreen/gfDanceTitlebg.png");
-    add(gfDanceTitlebg);
-    }
+	override public function create():Void
+	{
+		super.create();
+
+		gfDanceTitlebg = new FlxSprite(0, 0, "assets/images/menus/titlescreen/gfDanceTitlebg.png");
+		add(gfDanceTitlebg);
+
+		gfDanceTitle = new FlxAnimatedSprite(0, 0);
+		gfDanceTitle.loadGraphic("assets/images/menus/titlescreen/gfDance.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
+		gfDanceTitle.addAnimation("gfDance", [0, 1, 2, 3, 4, 5], 12, true);
+		gfDanceTitle.play("gfDance");
+		add(gfDanceTitle);
+
+		logo = new FlxAnimatedSprite(0, 0);
+		logo.loadGraphic("assets/images/menus/titlescreen/logo.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
+		logo.addAnimation("logo bumpin", [0, 1, 2, 3, 4, 5], 12, true);
+		logo.play("logo bumpin");
+		add(logo);
+
+		titleEnter = new FlxAnimatedSprite(0, 0);
+		titleEnter.loadGraphic("assets/images/menus/titlescreen/titleEnter.png", FlxAnimatedSprite.sparrowAtlas, 1, true, 0, 0);
+		titleEnter.addAnimation("ENTER PRESSED", [0, 1, 2, 3, 4, 5], 12, true);
+		titleEnter.addAnimation("ENTER FREEZE", [0], 12, true);
+		titleEnter.play("ENTER FREEZE");
+		add(titleEnter);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		if (!pressedEnter && checkEnterInput())
+		{
+			pressedEnter = true;
+			titleEnter.play("ENTER PRESSED");
+			onEnterPressed();
+		}
+	}
+
+	function checkEnterInput():Bool
+	{
+		#if mobile
+		for (touch in FlxG.touches.list)
+		{
+			if (touch.justPressed)
+				return true;
+		}
+		#end
+
+		#if !mobile
+		if (FlxG.keys.justPressed.ENTER)
+			return true;
+
+		if (FlxG.mouse.justPressed)
+			return true;
+		#end
+
+		return false;
+	}
+
+	function onEnterPressed():Void
+	{
+	}
 }
