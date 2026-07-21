4c46c3a77fb94f90e92b9238fde30bbf3946197f
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-21 10:27:03 -0300

    Initialize storage and setup game in Main class

--- source/Main.hx
+++ source/Main.hx
@@ -1,1 +1,71 @@
 
+package;
+
+import openfl.display.Sprite;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
+import openfl.events.Event;
+import flixel.FlxGame;
+
+import states.TitleState;
+import mobile.backend.StorageUtil;
+
+#if android
+import lime.system.System as LimeSystem;
+#end
+
+class Main extends Sprite
+{
+	public static var storageInitialized:Bool = false;
+
+	public function new()
+	{
+		super();
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+		}
+	}
+
+	private function onAddedToStage(e:Event):Void
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, onAddedToStage);
+		init();
+	}
+
+	private function init():Void
+	{
+		if (stage != null)
+		{
+			stage.align = StageAlign.TOP_LEFT;
+			stage.scaleMode = StageScaleMode.NO_SCALE;
+		}
+
+		setupStorage();
+		setupGame();
+	}
+
+	private function setupStorage():Void
+	{
+		#if android
+		StorageUtil.requestExternalStoragePermission();
+		#end
+
+		StorageUtil.ensureDirectory("mods");
+		StorageUtil.ensureDirectory("saves");
+		StorageUtil.ensureDirectory("logs");
+		StorageUtil.ensureDirectory("cache");
+
+		storageInitialized = true;
+	}
+
+	private function setupGame():Void
+	{
+		addChild(new FlxGame(0, 0, TitleState, 60, 60, true));
+	}
+}
