e8deef7e33f0deb62543a9514019bcfc7bc7957d
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-07-21 11:08:04 -0300

    Implement mobile hitbox in PlayState
    Added mobile-specific hitbox functionality to PlayState.

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2,15 +2,47 @@ package;
 
 import flixel.FlxState;
 
+#if mobile
+import mobile.controls.Hitbox;
+import mobile.controls.HitboxDirection;
+#end
+
 class PlayState extends FlxState
 {
+	#if mobile
+	var hitbox:Hitbox;
+	#end
+
 	override public function create()
 	{
 		super.create();
+
+		#if mobile
+		hitbox = new Hitbox();
+		add(hitbox);
+		#end
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		#if mobile
+		if (hitbox.justPressed(LEFT))
+		{
+		}
+
+		if (hitbox.justPressed(DOWN))
+		{
+		}
+
+		if (hitbox.justPressed(UP))
+		{
+		}
+
+		if (hitbox.justPressed(RIGHT))
+		{
+		}
+		#end
 	}
 }
